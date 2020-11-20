ddb2ccd4a396aecaa3d1ed90d95d100e7a9c3cce
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2020-11-20 18:08:06 +0000

    Add Sass

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.scss'
 
 export default function Home() {
   return (
